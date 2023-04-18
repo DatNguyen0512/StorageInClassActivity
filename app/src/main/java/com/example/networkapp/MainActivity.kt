300f6f57a81b3b820e872f24f1a671a53c86be38
Dat Nguyen <tuj81499@temple.edu>
2023-04-18 12:18:23 -0400

    fix savefile

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -15,11 +15,7 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
-import java.io.BufferedInputStream
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
-import java.io.IOException
+import java.io.*
 import java.util.prefs.AbstractPreferences
 
 private const val AUTO_SAVE_KEY = "auto_save"
@@ -58,7 +54,23 @@ class MainActivity : AppCompatActivity() {
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
 
+        if ( file.exists()){
+            try {
+                val br = BufferedReader(FileReader(file))
+                val text = StringBuilder()
+                var line: String?
+                while (br.readLine().also{ line = it }!= null){
+                    text.append(line)
+                }
+                br.close()
+                showComic(JSONObject(text.toString()))
+
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
         showButton.setOnClickListener {
+
             downloadComic(numberEditText.text.toString())
         }
 
@@ -68,21 +80,15 @@ class MainActivity : AppCompatActivity() {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
             JsonObjectRequest(url, {
-                if (autoSave && file.exists()){
-                    try {
-                        val br = BufferedReader(FileReader(file))
-                        val text = StringBuilder()
-                        var line: String?
-                        while (br.readLine().also{ line = it }!= null){
-                            text.append(line)
-                        }
-
-
-                    }catch (e: IOException){
-                        e.printStackTrace()
-                    }
+                try {
+                    val outputStream = FileOutputStream(file)
+                    outputStream.write(it.toString().toByteArray())
+                    outputStream.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
                 showComic(it)}, {
+
             })
         )
     }
